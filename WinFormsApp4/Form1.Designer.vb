9e580ff31d3655b22306885380133f3b3082d15e
WALDIR <WALDIR@DESKTOP-MOO23FQ>
2023-09-19 09:21:11 -0600

    radiobutton

--- WinFormsApp4/Form1.Designer.vb
+++ WinFormsApp4/Form1.Designer.vb
@@ -25,6 +25,7 @@ Partial Class Form1
         TextBox1 = New TextBox()
         TextBox2 = New TextBox()
         Button1 = New Button()
+        RadioButton1 = New RadioButton()
         SuspendLayout()
         ' 
         ' TextBox1
@@ -50,11 +51,23 @@ Partial Class Form1
         Button1.Text = "1"
         Button1.UseVisualStyleBackColor = True
         ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(204, 121)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(97, 19)
+        RadioButton1.TabIndex = 3
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "RadioButton1"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(RadioButton1)
         Controls.Add(Button1)
         Controls.Add(TextBox2)
         Controls.Add(TextBox1)
@@ -67,4 +80,5 @@ Partial Class Form1
     Friend WithEvents TextBox1 As TextBox
     Friend WithEvents TextBox2 As TextBox
     Friend WithEvents Button1 As Button
+    Friend WithEvents RadioButton1 As RadioButton
 End Class
